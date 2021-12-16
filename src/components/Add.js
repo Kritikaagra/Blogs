27b50f6ffcc4259e756684dec66d4878b7a01a3a
Kritika agrawal <kritikaagrawal75@gmail.com>
2021-12-16 14:50:20 +0530

    update preview

--- src/components/Add.js
+++ src/components/Add.js
@@ -10,20 +10,16 @@ function Add() {
   let history = useHistory();
   const [userInfo, setuserInfo] = useState({
     title: '',
-    description: '',
     information: '',
   });
+
   const onChangeValue = (e) => {
     setuserInfo({
       ...userInfo,
       [e.target.name]:e.target.value
     });
   } 
-  const ondescription = (value) => {
-    setuserInfo({ ...userInfo,
-      description:value
-    });
-  } 
+
   const oninformation = (value) => {
     setuserInfo({ ...userInfo,
       information:value
@@ -34,13 +30,8 @@ function Add() {
     try {
       event.preventDefault();
       event.persist();
-      if(userInfo.description.length < 50){
-        setError('Required, Add description minimum length 50 characters');
-        return;
-      }
       axios.post(`http://localhost:8080/addArticle`, {
         title: userInfo.title,
-        description: userInfo.description,
         information: userInfo.information,
       })
       .then(res => {
@@ -58,43 +49,42 @@ return (
     <div className="container">
       <div className="row"> 
         <form onSubmit={addDetails} className="update__forms">
-          <h3 className="myaccount-content"> Add  </h3>
+         
           <div className="form-row">
             <div className="form-group col-md-12">
-              <label className="font-weight-bold"> Title <span className="required"> * </span> </label>
-              <input type="text" name="title" value={userInfo.title} onChange={onChangeValue}  className="form-control" placeholder="Title" required />
+              <input type="text" name="title" value={userInfo.title} onChange={onChangeValue}  className="form-control" placeholder="(Whatever the topic will be)" required />
             </div>
-            <div className="clearfix"></div>
+           
             <div className="form-group col-md-12 editor">
-              <label className="font-weight-bold"> Description <span className="required"> * </span> </label>
             <EditorToolbar toolbarId={'t1'}/>
-            <ReactQuill
-              theme="snow"
-              value={userInfo.description}
-              onChange={ondescription}
-              placeholder={"Write something awesome..."}
-              modules={modules('t1')}
-              formats={formats}
-            />
-            </div>
-            <br />
-            <div className="form-group col-md-12 editor">
-              <label className="font-weight-bold"> Additional Information  </label>
-            <EditorToolbar toolbarId={'t2'}/>
             <ReactQuill
               theme="snow"
               value={userInfo.information}
               onChange={oninformation}
               placeholder={"Write something awesome..."}
-              modules={modules('t2')}
+              modules={modules('t1')}
               formats={formats}
             />
             </div>
-            <br />
+            
+            <div className="preview_heading">Preview</div>
+            <div className="blog_preview">
+            <ReactQuill
+              theme="bubble"
+              value={userInfo.information}
+              readOnly={true}
+              formats={formats}
+            />
+            </div>
+            
+            
             {isError !== null && <div className="errors"> {isError} </div>}
-            <div className="form-group col-sm-12 text-right">
-              <button type="submit" className="btn btn__theme"> Submit  </button>
-            </div> 
+            <div class="buttons">
+            <button type="button" class="btn btn-black">Back To All</button>
+            <button type="button" class="btn btn-red">Discard Changes</button>
+            <button type="button" class="btn btn-green">Save Changes</button>
+            </div>
+            
           </div> 
         </form>
       </div>
@@ -103,4 +93,6 @@ return (
 </>
 )
 }
+
+
 export default Add
